60b6fdf552e61bc72440e992cdfa7e221634afd3
Horror_Guy7 <ccsimmons28@riverdale.edu>
2025-02-14 08:59:46 -0800

    Updated progress on project

--- SnowDayCalculator.java
+++ SnowDayCalculator.java
@@ -1,18 +1,33 @@
 public class SnowDayCalculator {
     public static void main(String[] args) {
-        // isMountain, latitude, day, SinglaParentRate,temperatureMorning
+        /*HELLO USER! WELCOME TO THE SNOWDAY CALKULATOR! INPUT WHETHER YOU ARE IN A MOUNTANOUS
+        AREA, YOUR |LATITUDE| (0-90),  WHAT DAY IS IT (1-5), THE TEMPERATURE IN THE MORNING, THE PERCENTAGE
+        OF STUDENTS IN CLASS WHO HAVE DIVORCED PARENTS (%)
+        * */
         boolean isMountainous = Boolean.parseBoolean(args[0]);
         double latitude  = Double.parseDouble(args[1]);
         int day = Integer.parseInt(args[2]);
-        double SingleParentRate = Double.valueOf(args[3]);
+        double DivorceRate = Double.valueOf(args[3]);
         double totalPoints = 0;
         double temperatureMorning = Double.valueOf(args[4]);
+        // Validating Variables//
+
+        if (day <1 || day>5) {
+            System.out.println("请输入1至5之数");
+            return;
+        }
+        if (DivorceRate <0 ||DivorceRate>100) {
+            System.out.println("You need more divorce to have a snow day");
+            return;
+        }
+
+
 
         System.out.print("Are you in a Mountainous area? " + args[0]);
         System.out.print("\nWhat is your latitude? " + args[1]);
         System.out.print("\nWhat day is it? " + args[2]);
         System.out.print("\nWhat is the temperature this morning? " + args[3]);
-        System.out.print("\nWhat is the single parent rate right now? " + args[4]);
+        System.out.print("\nWhat is the divorce rate right now? " + args[4]+"%");
 
         if(isMountainous){
             totalPoints += 2;
@@ -24,15 +39,16 @@ public class SnowDayCalculator {
         if (temperatureMorning <= -10) {
             totalPoints +=3;
         }
-        if (SingleParentRate >= 30) {
+        if (DivorceRate >= 30) {
             totalPoints += 3;
 
         }
 
-        totalPoints += (latitude-90)/3;
-        totalPoints += 5*(totalPoints - 50);
+        totalPoints += (latitude-90)/3;// Latitude calc
+        String willSnow = ( totalPoints >=10) ? "¡Hay un día de nieve hoy!": "Hay que ir a la escuela";
         System.out.println(" \nYour total points add up to " +totalPoints + "\nCalculat" +
-                "ing chances..." + "\n..."+"\n..."+"\n..."+"\n...");
+                "ing chances..." + "\n..."+"\n..."+"\n..."+"\n..." + "\nThe result is: " + willSnow);
+
         }
 
     }
